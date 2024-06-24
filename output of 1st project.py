bb90f2cf896d2317e02a356c93d6d64ea10009b8
jj123184 <mohdanabtawi2006@gmail.com>
2024-06-24 17:43:56 +0400

    Add files via upload

---
+++ output of 1st project.py
@@ -0,0 +1,50 @@
+Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================== RESTART: /Users/mohammad.anabtawi/Documents/1st project.py =================
+Enter your age in years:
+67
+You are at least 24455.0 days old
+You are at least 3484.0 weeks old
+You are at least 67.0 years old
+Older Adults
+>>> 
+================== RESTART: /Users/mohammad.anabtawi/Documents/1st project.py =================
+Enter your age in years:
+0.0575
+You are at least 20.9875 days old
+You are at least 2.99 weeks old
+You are at least 0.0575 years old
+Neonates or newborns
+>>> 
+================== RESTART: /Users/mohammad.anabtawi/Documents/1st project.py =================
+Enter your age in years:
+0.667
+You are at least 243.455 days old
+You are at least 34.684000000000005 weeks old
+You are at least 0.667 years old
+Infants
+>>> 
+================== RESTART: /Users/mohammad.anabtawi/Documents/1st project.py =================
+Enter your age in years:
+5
+You are at least 1825.0 days old
+You are at least 260.0 weeks old
+You are at least 5.0 years old
+Children
+>>> 
+================== RESTART: /Users/mohammad.anabtawi/Documents/1st project.py =================
+Enter your age in years:
+22
+You are at least 8030.0 days old
+You are at least 1144.0 weeks old
+You are at least 22.0 years old
+Adults
+>>> 
+================== RESTART: /Users/mohammad.anabtawi/Documents/1st project.py =================
+Enter your age in years:
+16
+You are at least 5840.0 days old
+You are at least 832.0 weeks old
+You are at least 16.0 years old
+Adolescents
